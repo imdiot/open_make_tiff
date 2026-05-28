d2ee2d99b3ecaade9fb48bdaff2b131415e76419
imdiot <imdiot.y@gmail.com>
2026-05-28 11:26:26 +0800

    feat(icc): add Flextight Input ICC profile

--- pkg/icc/icc.go
+++ pkg/icc/icc.go
@@ -9,6 +9,8 @@ var (
 	_BT2020 []byte
 	//go:embed DisplayP3.icc
 	_DisplayP3 []byte
+	//go:embed FlextightInput.icc
+	_FlextightInput []byte
 	//go:embed HasselbladRGB.icc
 	_HasselbladRGB []byte
 	//go:embed ProPhoto.icm
@@ -18,12 +20,13 @@ var (
 )
 
 var Profiles = map[string]*Profile{
-	"AdobeRGB1998":  {Name: "Adobe RGB 1998", Data: _AdobeRGB1998},
-	"BT2020":        {Name: "BT.2020", Data: _BT2020},
-	"DisplayP3":     {Name: "Display P3", Data: _DisplayP3},
-	"HasselbladRGB": {Name: "Hasselblad RGB", Data: _HasselbladRGB},
-	"ProPhoto":      {Name: "ProPhoto", Data: _ProPhoto},
-	"sRGB":          {Name: "sRGB", Data: _sRGB},
+	"AdobeRGB1998":   {Name: "Adobe RGB 1998", Data: _AdobeRGB1998},
+	"BT2020":         {Name: "BT.2020", Data: _BT2020},
+	"DisplayP3":      {Name: "Display P3", Data: _DisplayP3},
+	"FlextightInput": {Name: "Flextight Input", Data: _FlextightInput},
+	"HasselbladRGB":  {Name: "Hasselblad RGB", Data: _HasselbladRGB},
+	"ProPhoto":       {Name: "ProPhoto", Data: _ProPhoto},
+	"sRGB":           {Name: "sRGB", Data: _sRGB},
 }
 
 type Profile struct {
